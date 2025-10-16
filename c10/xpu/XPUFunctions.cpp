645eb2a117cc965cefa1de6ae16826f1f5b13f4c
Yu, Guangye <guangye.yu@intel.com>
2025-10-16 16:55:52 +0000

    Remove unused parameter when query extension attribute
    ghstack-source-id: 30c9385d224030001ad0b5f9c92a8eb4841b21dc
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/165623

--- c10/xpu/XPUFunctions.cpp
+++ c10/xpu/XPUFunctions.cpp
@@ -138,16 +138,6 @@ inline void initGlobalDevicePoolState() {
     }
   }
 
-#if defined(_WIN32) && SYCL_COMPILER_VERSION < 20250000
-  // The default context feature is disabled by default on Windows for SYCL
-  // compiler versions earlier than 2025.0.0.
-  std::vector<sycl::device> deviceList;
-  for (auto it = gDevicePool.devices.begin(); it != gDevicePool.devices.end();
-       ++it) {
-    deviceList.push_back(*(*it));
-  }
-  gDevicePool.context = std::make_unique<sycl::context>(deviceList);
-#else
   // The default context is utilized for each Intel GPU device, allowing the
   // retrieval of the context from any GPU device.
   const auto& platform = gDevicePool.devices[0]->get_platform();
@@ -156,7 +146,6 @@ inline void initGlobalDevicePoolState() {
       platform.khr_get_default_context());
 #else
       platform.ext_oneapi_get_default_context());
-#endif
 #endif
 }
 
@@ -184,7 +173,7 @@ void initDeviceProperties(DeviceProp* device_prop, DeviceIndex device) {
 
 #define ASSIGN_EXP_CL_ASPECT(member)                                       \
   device_prop->has_##member = raw_device.ext_oneapi_supports_cl_extension( \
-      "cl_intel_" #member, &cl_version);
+      "cl_intel_" #member);
 
 #define ASSIGN_EXP_DEVICE_PROP(property) \
   device_prop->property =                \
@@ -199,8 +188,6 @@ void initDeviceProperties(DeviceProp* device_prop, DeviceIndex device) {
 
   AT_FORALL_XPU_DEVICE_ASPECT(ASSIGN_DEVICE_ASPECT);
 
-  // TODO: Remove cl_version since it is unnecessary.
-  sycl::ext::oneapi::experimental::cl_version cl_version;
   AT_FORALL_XPU_EXP_CL_ASPECT(ASSIGN_EXP_CL_ASPECT);
 
 #if SYCL_COMPILER_VERSION >= 20250000
